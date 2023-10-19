5a009f1e559b4a25965fe9cc5faf0d7a1ee49515
Ivanek-droid <88985338+Ivanek-droid@users.noreply.github.com>
2023-10-19 20:28:08 +0200

    Pridani sekce num a reseni pole polí

--- Maturitni__projekt.ino
+++ Maturitni__projekt.ino
@@ -473,6 +473,7 @@ byte nine[8] =
    0b00000000,
    0b00000000,
 };
+byte sign[][] ={I,V,A,N, ,H,V,E,Z,D,A};
 
 String slovo = "Dominik je curak";
 
@@ -531,19 +532,229 @@ void setup(){
 }
    ///////////////////////////////////////////////////////////////////////////////////////////loop///////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop(){
-  
+/*
 ascii();
 abc();
 full();
 none();
+*/
+num();
 
+}
  ///////////////////////////////////////////////////////////////////////////////////////////Seriova komunikace///////////////////////////////////////////////////////////////////////////////////////////////////////
 
    ///////////////////////////////////////////////////////////////////////////////////////////abc - vypsani vsech pisnem z abecedy rezim///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void num(){
+for(int j = 0;j<=10;j++){
+ for (int k = 0; k <= 7; k++) {
+   for (int l = 0; l <= 100; l++) {
+     for (int i = 0; i <= 7; i++) {
+
+
+
+   switch(j){
+    case 0:
+    currentElement = zero[i];
+    break;
+    case 1:
+    currentElement = one[i];
+    break;
+    case 2:
+    currentElement = two[i];
+    break;
+    case 3:
+    currentElement = three[i];
+    break;
+    case 4:
+    currentElement = four[i];
+    break;
+    case 5:
+    currentElement = five[i];
+    break;
+    case 6:
+    currentElement = six[i];
+    break;
+    case 7:
+    currentElement = seven[i];
+    break;
+    case 8:
+    currentElement = eight[i];
+    break;
+    case 9:
+    currentElement = nine[i];
+    break;
+  }
+  switch(k){
+    case 0:
+     
+    digitalWrite(latchPin1,0);
+    shiftOut(dataPin1,clockPin1,LSBFIRST,currentElement);
+    digitalWrite(latchPin1,1);
+    
+     digitalWrite(latchPin1,0);
+    shiftOut(dataPin1,clockPin1,LSBFIRST,nott);
+    digitalWrite(latchPin1,1);
+    break;
+    case 1:
+    digitalWrite(latchPin2,0);
+    shiftOut(dataPin2,clockPin2,LSBFIRST,currentElement);
+    digitalWrite(latchPin2,1);
+
+    digitalWrite(latchPin2,0);
+    shiftOut(dataPin2,clockPin2,LSBFIRST,nott);
+    digitalWrite(latchPin2,1);
+    break;
+    case 2:
+    digitalWrite(latchPin3,0);
+    shiftOut(dataPin3,clockPin3,LSBFIRST,currentElement);
+    digitalWrite(latchPin3,1);
+
+    digitalWrite(latchPin3,0);
+    shiftOut(dataPin3,clockPin3,LSBFIRST,nott);
+    digitalWrite(latchPin3,1);
+    break;
+    case 3:
+    digitalWrite(latchPin4,0);
+    shiftOut(dataPin4,clockPin4,LSBFIRST,currentElement);
+    digitalWrite(latchPin4,1);
+
+    digitalWrite(latchPin4,0);
+    shiftOut(dataPin4,clockPin4,LSBFIRST,nott);
+    digitalWrite(latchPin4,1);
+    break;
+    case 4:
+    digitalWrite(latchPin5,0);
+    shiftOut(dataPin5,clockPin5,LSBFIRST,currentElement);
+    digitalWrite(latchPin5,1);
+
+    digitalWrite(latchPin5,0);
+    shiftOut(dataPin5,clockPin5,LSBFIRST,nott);
+    digitalWrite(latchPin5,1);
+    break;
+    case 5:
+    digitalWrite(latchPin6,0);
+    shiftOut(dataPin6,clockPin6,LSBFIRST,currentElement);
+    digitalWrite(latchPin6,1);
+
+    digitalWrite(latchPin6,0);
+    shiftOut(dataPin6,clockPin6,LSBFIRST,nott);
+    digitalWrite(latchPin6,1);
+    break;
+    case 6:
+    digitalWrite(latchPin7,0);
+    shiftOut(dataPin7,clockPin7,LSBFIRST,currentElement);
+    digitalWrite(latchPin7,1);
+
+    digitalWrite(latchPin7,0);
+    shiftOut(dataPin7,clockPin7,LSBFIRST,nott);
+    digitalWrite(latchPin7,1);
+    break;
+    case 7:
+    digitalWrite(latchPin8,0);
+    shiftOut(dataPin8,clockPin8,LSBFIRST,currentElement);
+    digitalWrite(latchPin8,1);
+
+    digitalWrite(latchPin8,0);
+    shiftOut(dataPin8,clockPin8,LSBFIRST,nott);
+    digitalWrite(latchPin8,1);
+    break;
+
+  }
+    
+    switch(i){
+    case 0:
+    digitalWrite(layers[0],0);
+    digitalWrite(layers[1],1);
+    digitalWrite(layers[2],1);
+    digitalWrite(layers[3],1);
+    digitalWrite(layers[4],1);
+    digitalWrite(layers[5],1);
+    digitalWrite(layers[6],1);
+    digitalWrite(layers[7],1);
+    break;
+    case 1:
+digitalWrite(layers[0],1);
+    digitalWrite(layers[1],0);
+    digitalWrite(layers[2],1);
+    digitalWrite(layers[3],1);
+    digitalWrite(layers[4],1);
+    digitalWrite(layers[5],1);
+    digitalWrite(layers[6],1);
+    digitalWrite(layers[7],1);
+    break;
+    case 2:
+digitalWrite(layers[0],1);
+    digitalWrite(layers[1],1);
+    digitalWrite(layers[2],0);
+    digitalWrite(layers[3],1);
+    digitalWrite(layers[4],1);
+    digitalWrite(layers[5],1);
+    digitalWrite(layers[6],1);
+    digitalWrite(layers[7],1);
+    break;
+    case 3:
+digitalWrite(layers[0],1);
+    digitalWrite(layers[1],1);
+    digitalWrite(layers[2],1);
+    digitalWrite(layers[3],0);
+    digitalWrite(layers[4],1);
+    digitalWrite(layers[5],1);
+    digitalWrite(layers[6],1);
+    digitalWrite(layers[7],1);
+    break;
+    case 4:
+digitalWrite(layers[0],1);
+    digitalWrite(layers[1],1);
+    digitalWrite(layers[2],1);
+    digitalWrite(layers[3],1);
+    digitalWrite(layers[4],0);
+    digitalWrite(layers[5],1);
+    digitalWrite(layers[6],1);
+    digitalWrite(layers[7],1);
+    break;
+    case 5:
+digitalWrite(layers[0],1);
+    digitalWrite(layers[1],1);
+    digitalWrite(layers[2],1);
+    digitalWrite(layers[3],1);
+    digitalWrite(layers[4],1);
+    digitalWrite(layers[5],0);
+    digitalWrite(layers[6],1);
+    digitalWrite(layers[7],1);
+    break;
+    case 6:
+digitalWrite(layers[0],1);
+    digitalWrite(layers[1],1);
+    digitalWrite(layers[2],1);
+    digitalWrite(layers[3],1);
+    digitalWrite(layers[4],1);
+    digitalWrite(layers[5],1);
+    digitalWrite(layers[6],0);
+    digitalWrite(layers[7],1);
+    break;
+    case 7:
+digitalWrite(layers[0],1);
+    digitalWrite(layers[1],1);
+    digitalWrite(layers[2],1);
+    digitalWrite(layers[3],1);
+    digitalWrite(layers[4],1);
+    digitalWrite(layers[5],1);
+    digitalWrite(layers[6],1);
+    digitalWrite(layers[7],0);
+    break;
+
+}
+    //delay(5);
+  }
+} 
+}
+}
+}
 void abc(){
 
 
-for(int j = 0;j<=36;j++){
+for(int j = 0;j<=26;j++){
  for (int k = 0; k <= 7; k++) {
    for (int l = 0; l <= 100; l++) {
      for (int i = 0; i <= 7; i++) {
@@ -628,36 +839,6 @@ for(int j = 0;j<=36;j++){
     break;
     case 26:
     currentElement = Z[i];
-    break;
-    case 27:
-    currentElement = zero[i];
-    break;
-    case 28:
-    currentElement = one[i];
-    break;
-    case 29:
-    currentElement = two[i];
-    break;
-    case 30:
-    currentElement = three[i];
-    break;
-    case 31:
-    currentElement = four[i];
-    break;
-    case 32:
-    currentElement = five[i];
-    break;
-    case 33:
-    currentElement = six[i];
-    break;
-    case 34:
-    currentElement = seven[i];
-    break;
-    case 35:
-    currentElement = eight[i];
-    break;
-    case 36:
-    currentElement = nine[i];
     break;
   }
   switch(k){
